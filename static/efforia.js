b9d7d8371452c55604be9b2211505afd649c16c6
Vicente Fernandes <vicente_l_f@hotmail.com>
2011-12-12 11:34:12 -0200

    Updated helix compatibility

--- static/efforia.js
+++ static/efforia.js
@@ -1,40 +1,89 @@
 function init() {
-	(function() {
-		if (!window.requestAnimationFrame ) {
-			window.requestAnimationFrame = (function() {
-				return window.webkitRequestAnimationFrame ||
-        		window.mozRequestAnimationFrame ||
-        		window.oRequestAnimationFrame ||
-        		window.msRequestAnimationFrame ||
-        		function(callback,element) {
-          			window.setTimeout(callback,1000/60);
-        		};
-      		})();
+	(function()
+	{
+		if (!window.requestAnimationFrame )
+		{
+			window.requestAnimationFrame = (function() 
+			{
+				return window.webkitRequestAnimationFrame 	|| 	
+					   window.mozRequestAnimationFrame 		|| 	
+				       window.oRequestAnimationFrame 		|| 	
+					   window.msRequestAnimationFrame	 	||
+					   function(callback,element)
+					   {	window.setTimeout(callback,1000/60); };
+      		})();	
     	}
 		var canvas = new fabric.Canvas('efforia');
-		setTimeout(drawShape(),1000);
+		drawShape();
 		function drawShape() {
-			fabric.loadSVGFromURL('/static/interface.svg', function(objects,options) {
+			fabric.loadSVGFromURL('interface.svg', function(objects,options) {
 				var helix = new fabric.PathGroup(objects,options);
 				helix.set({
               		//left: coords[i].x,
               		//top: coords[i].y,
               		//angle: 30,
-              		//fill: '#ff5555'
+            	});
+            	var rect = new fabric.Rect({
+            		top: 100,
+            		left: 100,
+            		fill: 'blue',
+            		width: 20,
+            		height: 20
             	});
 				canvas.add(helix);
+				canvas.add(rect);
+				
   				canvas.centerObjectH(helix).centerObjectV(helix);
-  				//canvas.centerObjectH(helix);
+  				canvas.item(0).hasBorders = false;
+  				canvas.item(0).lockScalingX = canvas.item(0).lockScalingY = true;
+  				canvas.item(0).lockMovementY = canvas.item(0).lockMovementX = true;
+				
+  				canvas.observe({
+    				'mouse:down': function(e) {
+      					if (e.memo.target) {
+        					e.memo.target.opacity = 0.5;
+        					canvas.renderAll();
+      					}
+    				},
+    				'mouse:up': function(e) {
+      					if (e.memo.target) {
+        					e.memo.target.opacity = 1;
+        					canvas.renderAll();
+      					}
+    				},
+    				'object:moved': function(e) {
+      					e.memo.target.opacity = 0.5;
+    				},
+    				'object:modified': function(e) {
+      					e.memo.target.opacity = 1;
+    				}
+  				});
+  				canvas.item(0).lockMovementY = true;
+  				canvas.item(0).lockMovementX = true;
+				//canvas.item(0).selectable = false;// helice nao pode ser selecionada
+  				canvas.selection = false;//group selection desabilitado
   				animate();
 			});
 		}
 		var angle = 0;
+		//var time = new Date().getTime() * 0.002;
+    	//var x = Math.sin( time ) * 96 + 128;
+    	//var y = Math.cos( time ) * 96 + 128;
 		function animate() {
-			angle += 2;
+			angle += 1;
       		if (angle === 360) {
         		angle = 0;
       		}
+      		var x = Math.sin( time ) * 96 + 128;
+    		var y = Math.cos( time ) * 96 + 128;
       		canvas.item(0).setAngle(angle);
+      		var time = new Date().getTime() * 0.002;
+      		
+      		canvas.item(1).setOptions({
+      			top: x,
+            	left: y
+      		});
+      		
       		canvas.renderAll();
         	window.requestAnimationFrame(animate);
       	} 
