437e0e75316ae56e7464b8b25332d5eaa81237ea
William <william@william-A790GXM-AD3.(none)>
2012-01-31 15:06:06 -0200

    Added local django settings

--- efforia/settings.py
+++ efforia/settings.py
@@ -1,3 +1,6 @@
+DATABASE_ENGINE='django.db.backends.postgresql_psycopg2'
+DATABASE_NAME='efforia.db'
+
 INSTALLED_APPS=('play','spread',
 		'django.contrib.contenttypes',
                 'django.contrib.auth',
