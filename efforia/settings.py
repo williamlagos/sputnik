d968f9da7a1eae8f826902ebac84aa65204538a9
William <william@william-A790GXM-AD3.(none)>
2012-01-31 13:27:52 -0200

    Removed database locals from efforia django settings

--- efforia/settings.py
+++ efforia/settings.py
@@ -36,8 +36,6 @@ try:
 except Exception:
     print 'Unexpected error:', sys.exc_info()
 
-DATABASE_ENGINE='django.db.backends.postgresql_psycopg2'
-DATABASE_NAME='efforia.db'
 INSTALLED_APPS=('play','spread',
 		'django.contrib.contenttypes',
                 'django.contrib.auth',
