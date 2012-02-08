a42c30cdeb5b5091cd6a8440fbd5ce7d316b2368
William <william@william-A790GXM-AD3.(none)>
2012-02-08 18:35:43 -0200

    Added first YouTube embedded player

--- efforia/play/video.py
+++ efforia/play/video.py
@@ -1,5 +1,10 @@
 from base import BaseHandler
+import os,sys
+sys.path.append(os.path.abspath(".."))
+from spread import models,social
 
-class PlayerHandler(BaseHandler):
+class PlayerHandler(social.SocialHandler):
     def get(self):
-        return self.render(self.templates()+'play.html')
+	user = self.current_user()
+	known = self.current_relations()
+        return self.render(self.templates()+'play.html',user=user,known=known)
