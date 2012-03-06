ac92c5977504322f6e9ca9633296a7a71d57a2cf
William <william@william-A790GXM-AD3.(none)>
2012-03-06 18:26:48 -0300

    Fixed problems in helix

---
+++ static/efforia.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+
+$( "#dialogo" ).dialog({height:'auto',width:'auto',modal:true});
+$( "input:submit, a, button", "#botoes" ).button();
+$('#conteudoCanvas').bind("click",function(){});
+$('.quadradoFlip').bind("click",function(){
+	var elem = $(this);
+	if(elem.data('flipped')){
+		elem.revertFlip();
+		elem.data('flipped',false)
+	}else{
+		elem.flip({
+			direction:'lr',
+			speed: 350,
+			onBefore: function(){ elem.html(elem.siblings('.dadosQuadrado').html()); }
+		});
+		elem.data('flipped',true);
+	}
+});
+
+$('#conteudoCanvas').bind("dblclick",function(){
+	$('#conteudoCanvas:visible').hide('fade');
+	$('#ferramentas:visible').hide('fade'); 
+});
+
+$('#conteudoCentral').bind("dblclick",function(){ 
+	$('#conteudoCanvas:hidden').show('fade');
+	$('#ferramentas:hidden').show('fade');
+});
+
+$('#conteudoCanvas').bind('mouseenter',function(){
+	$('#conteudoEsquerda:visible').hide('fade');
+    $('#conteudoDireita:visible').hide('fade');
+});
+
+$('#conteudoCanvas').bind('mouseleave',function(){
+	$('#conteudoEsquerda:hidden').show('fade');
+    $('#conteudoDireita:hidden').show('fade');
+});
+
+var w = window.innerWidth*0.7;
+var h = window.innerHeight;
+document.getElementById('efforia').width = w;
+document.getElementById('efforia').height = h;
+
+var canvas = new fabric.Canvas('efforia');
+var radians = 180/Math.PI; 
+var cX = canvas.width/2;
+var cY = canvas.height/2;
+var last = 0;
+var velocity = 0.001;
+var acceleration = 0.1;
+var clicked = justClicked = false;
+
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (function() 
+	{
+		return window.webkitRequestAnimationFrame 	|| 	
+			   window.mozRequestAnimationFrame 		|| 	
+		       window.oRequestAnimationFrame 		|| 	
+			   window.msRequestAnimationFrame	 	||
+			   function(callback,element)
+			   { window.setTimeout(callback,1000/60); };
+  	})();
+}
+
+drawElements();
+function drawElements() 
+{
+	fabric.loadSVGFromURL('interface.svg', function(objects,options) 
+	{
+		helix = new fabric.PathGroup(objects);
+		scaleFactor = h/900;
+		helix.scale(scaleFactor);
+		canvas.add(helix);
+		canvas.centerObjectH(helix).centerObjectV(helix);
+		canvas.selection = false;
+		canvas.forEachObject(function(obj) {
+			obj.hasBorders = obj.hasControls = false;
+			obj.lockScalingX = obj.lockScalingY = true;
+			obj.lockMovementX = obj.lockMovementY = true;	
+		});
+		listenEvents();
+	});
+}
+
+function listenEvents()
+{
+	canvas.observe('mouse:down',function(e) { clicked = true; justClicked = true; });
+	canvas.observe('mouse:up'  ,function(e) { clicked = false; });
+	canvas.observe('mouse:move',function(e) 
+	{
+		if (clicked) {
+			justClicked = false;
+			pos = canvas.getPointer(e.memo.e);
+			x = (cX)-pos.x; y = (cY)-pos.y;
+			if (velocity < 0) velocity = -velocity;
+			if (velocity >= last) {
+				last = velocity;
+				velocity = -(Math.atan(y/x)/radians);
+				velocity -= acceleration;
+			} else if (velocity < last) {
+				last = velocity;
+				velocity = Math.atan(y/x)/radians;
+				velocity += acceleration;
+			}
+			if (x <= cX && y <= cY)	velocity = -velocity;	
+			helix.theta += velocity;
+		}
+	});
+	date = new Date();
+	time = date.getTime();  
+	animateElements(time);	
+}
+
+function animateElements(lastTime)
+{
+	date = new Date();
+    time = date.getTime();
+    timeDiff = time - lastTime;
+   	if (!clicked && !justClicked) {
+    	angularFriction = 0.5;
+    	angularVelocity = velocity*timeDiff*(1-angularFriction)/1000;
+    	velocity -= angularVelocity;
+        helix.theta += velocity;
+    }
+	helix.angle = helix.theta*radians;
+	canvas.renderAll();
+	window.requestAnimationFrame(function() { animateElements(time); });
+}
+
+});
