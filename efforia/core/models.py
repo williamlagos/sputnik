1aef4c786bb42fc91fd16ed233123a8291a0b104
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-03-22 16:48:34 -0300

    Added tokens values to UserProfile model

--- efforia/core/models.py
+++ efforia/core/models.py
@@ -1,9 +1,12 @@
-from django.db.models import ForeignKey,IntegerField,Model
+from django.db.models import ForeignKey,IntegerField,Model,CharField
 from django.contrib.auth.models import User
 
 class UserProfile(Model):
     user = ForeignKey(User,unique=True)
     points = IntegerField(default=0)
     age = IntegerField(default=0)
+    google_token = CharField(default="",max_length=60)
+    twitter_token = CharField(default="",max_length=60)
+    facebook_token = CharField(default="",max_length=120)
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
