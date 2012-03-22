c4f436fdafdbc554babf10616483316d021ac3ca
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-03-22 17:34:14 -0300

    Added again some rows in the table

--- efforia/core/models.py
+++ efforia/core/models.py
@@ -5,6 +5,8 @@ class UserProfile(Model):
     user = ForeignKey(User,unique=True)
     points = IntegerField(default=0)
     age = IntegerField(default=0)
-    
+    google_token = CharField(default="",max_length=60)
+    twitter_token = CharField(default="",max_length=60)
+    facebook_token = CharField(default="",max_length=120)
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
