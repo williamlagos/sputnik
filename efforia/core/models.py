f6802e071fe14a1dd5667a79cdce67669c073717
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-03-23 02:13:36 -0300

    Removed some fields

--- efforia/core/models.py
+++ efforia/core/models.py
@@ -5,8 +5,6 @@ class UserProfile(Model):
     user = ForeignKey(User,unique=True)
     points = IntegerField(default=0)
     age = IntegerField(default=0)
-    google_token = TextField(default="",max_length=60)
-    twitter_token = TextField(default="",max_length=120)
-    facebook_token = TextField(default="",max_length=120)
+    
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
