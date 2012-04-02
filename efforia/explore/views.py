aa32cb159112ea831cc217ee11fcf72d21663c9c
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-04-02 14:37:47 -0300

    Added some dict code on response in CalendarHandler

--- efforia/explore/views.py
+++ efforia/explore/views.py
@@ -31,4 +31,5 @@ class CalendarHandler(SocialHandler,FacebookGraphMixin):
         self.facebook_request("/me/events",access_token=token,callback=self.async_callback(self._on_response))
     @tornado.web.asynchronous
     def _on_response(self,response):
-        return self.srender('calendar.html',response=response)
+        resp = response['data']
+        return self.srender('calendar.html',response=resp)
