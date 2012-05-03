22706c9ec0cfa1a3b419acca91332b1632a6bc36
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-05-03 14:20:26 -0300

    Added new fields to core UserProfile model

--- efforia/core/models.py
+++ efforia/core/models.py
@@ -5,6 +5,11 @@ from datetime import date
 class UserProfile(Model):
     user = ForeignKey(User,unique=True)
     points = IntegerField(default=0)
-
+    credit = IntegerField(default=0)
+    visual = CharField(default="",max_length=30)
+    birthday = DateTimeField(default=date.today())
+    google_token = TextField(default="",max_length=120)
+    twitter_token = TextField(default="",max_length=120)
+    facebook_token = TextField(default="",max_length=120)
     
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
