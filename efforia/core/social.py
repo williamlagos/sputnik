a8bfc102d5daa4aecf0f76fdb9df2e290dadec7f
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-06-06 03:21:28 -0300

    Refreshed stream service for Efforia with Google OAuth 2.0

--- efforia/core/social.py
+++ efforia/core/social.py
@@ -40,6 +40,7 @@ class GoogleHandler(tornado.web.RequestHandler,
                 client_id =     google_api['client_id'],
                 client_secret = google_api['client_secret'],
                 code =          self.get_argument("code"))
+            print token
             self.redirect("register?google_token=%s" % json.loads(token)['access_token'])
             return
         self.authorize_redirect(google_api['client_id'],
