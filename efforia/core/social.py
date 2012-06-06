3b44c67d21f22fb2abe0d163458941b4d286fb9f
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-06-06 03:52:52 -0300

    Refreshed stream service for Efforia with Google OAuth 2.0

--- efforia/core/social.py
+++ efforia/core/social.py
@@ -23,7 +23,10 @@ class GoogleOAuth2Mixin():
             'redirect_uri':  redirect_uri,
             'grant_type':    google_api['grant_type']
         })
-        return self.google_request(google_api['oauth2_token_url']+'?'+data)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        return self.google_request(google_api['oauth2_token_url'],str(data),headers)
     def google_request(self,url,body=u'',headers={},method='POST'):
         client = Client()
         response = client.fetch(Request(url,method,headers,body))
