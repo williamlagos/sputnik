c1a9d5f7b8f7d001fe73b9bfb9aeb6f10db9c1fd
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-06-14 17:47:42 -0300

    Fixed address of oauth_authenticate in TwitterHandler

--- efforia/core/social.py
+++ efforia/core/social.py
@@ -91,7 +91,7 @@ class TwitterHandler(tornado.web.RequestHandler,tornado.auth.TwitterMixin):
         if self.get_argument("oauth_token", None):
             self.get_authenticated_user(self.async_callback(self._on_auth))
             return
-        self.authorize_redirect()
+        self.authenticate_redirect()
     def _on_auth(self, user):
         if not user:
             raise tornado.web.HTTPError(500,"Twitter auth failed")
