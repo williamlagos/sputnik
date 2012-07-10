fc18023e1005b3489af8badd4ffdf773051c986e
William Oliveira de Lagos <william.lagos@live.com>
2012-07-10 16:09:10 -0300

    Added new database to Efforia

--- efforia/settings.py
+++ efforia/settings.py
@@ -1,5 +1,13 @@
-DATABASE_ENGINE='django.db.backends.postgresql_psycopg2'
-DATABASE_NAME='efforia'
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'd87i80lg975lcv',
+    'HOST': 'ec2-23-23-201-251.compute-1.amazonaws.com',
+    'PORT': 5432,
+    'USER': 'vcrkfqjrxpdhgy',
+    'PASSWORD': 'lFDRg2mdPpZvf3ya_b-cjbjx_I'
+  }
+}
 ROOT_URLCONF='urls'
 INSTALLED_APPS=('core','play','spread','create','store','explore','south',
 				'paypal.standard.ipn',
