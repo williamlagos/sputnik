e99734152deaa35473e663cb63a33dfd2c351f4c
William Oliveira de Lagos <william.lagos@live.com>
2012-08-23 11:54:19 -0300

    Changed database to production

--- efforia/settings.py
+++ efforia/settings.py
@@ -1,17 +1,17 @@
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'd8lj2qraq5jbrt',
-    'HOST': 'ec2-23-23-234-207.compute-1.amazonaws.com',
+    'NAME': 'd87i80lg975lcv',
+    'HOST': 'ec2-23-23-201-251.compute-1.amazonaws.com',
     'PORT': 5432,
-    'USER': 'edjzbpcwxlmfyv',
-    'PASSWORD': 'vhm_pQhDcjoux0iELZDH4p5-cn'
+    'USER': 'vcrkfqjrxpdhgy',
+    'PASSWORD': 'lFDRg2mdPpZvf3ya_b-cjbjx_I'
   }
 }
 ROOT_URLCONF='urls'
 INSTALLED_APPS=('core','play','spread','create','store','explore','south',
-				'paypal.standard.ipn',
-				'django.contrib.contenttypes',
+                'paypal.standard.ipn',
+                'django.contrib.contenttypes',
                 'django.contrib.auth',
                 'django.contrib.sessions')
 
