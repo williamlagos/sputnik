ff0ea7f42e3e4acdf4ac47ad30aa0206cdad7d7b
Fábrica de Ideias Coworking <fabrica@fabrica-desktop.(none)>
2012-11-03 19:09:03 -0200

    Fixed urls.py

--- efforia/urls.py
+++ efforia/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-		      (r'^discharge','store.views.discharge'))
-		      (r'^recharge','store.views.recharge'))
+		      (r'^discharge','store.views.discharge'),
+		      (r'^recharge','store.views.recharge'),
 		      (r'^balance','store.views.balance'))
 urlpatterns += staticfiles_urlpatterns()
