80c10f99b11106c5af5d99040362d2ccc6a401f6
William Oliveira de Lagos <william.lagos@live.com>
2012-11-16 17:24:23 -0200

    Fixed settings on Django

--- efforia/settings.py
+++ efforia/settings.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'paypal.standard.ipn','gunicorn','south'
+    'paypal.standard.ipn','gunicorn','south',
     'core','play','spread','create','store','explore',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
