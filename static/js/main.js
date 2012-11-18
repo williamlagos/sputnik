6e6b2f55ed318f23c2ff4461befe3177cdf664cc
William Oliveira de Lagos <william.lagos@ig.com.br>
2012-11-18 01:20:56 -0200

    Added latest changes on Efforia Stable

--- static/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready ->
-  $('.btn').on 'click', (event) ->
-    event.preventDefault()
-    $.get 'enter', $('.navbar-form').serialize(), (data) ->
-      console.log JSON.stringify(data)
-      console.log data['username']
-      
