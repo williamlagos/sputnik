028e68f5aa847dd20c716e837be365ed0c6310ce
Efforia Tecnologia <efforia@queenannesrevenge.home>
2015-09-18 19:41:16 -0300

    Added missing files for new app

---
+++ hub/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for hub project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hub.settings'
+application = Cling(get_wsgi_application())
